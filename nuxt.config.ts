063658d55ed7a6d477498d58115cff69b8ec22c8
Alexander David Fânaț-Dănuț <oaVa-o@outlook.com>
2025-04-17 15:15:38 +0300

    Add jsdom and related types for improved DOM manipulation in SSR. Update nuxt.config.ts to include Vite plugins. Refactor index.vue for enhanced site loading and selection handling.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
-  ssr: false,
-  devtools: { enabled: true }
+	compatibilityDate: '2024-11-01',
+	ssr: false,
+	devtools: { enabled: true },
+	vite: {
+		plugins: [
+		]
+	}
 })
--- pages/index.vue
+++ pages/index.vue
@@ -9,41 +9,56 @@
         <label for="selector">Selector</label>
         <input type="text" id="selector" name="selector" v-model="selector" />
         <br />
-        {{ site_holder }} 
-        <template v-for="holder in selection_holders" :key="holder.id">{{ holder }}</template>
+        <div ref="site_dock" />
+        <div v-for="[selection_identifier] in selection_holders" :key="selection_identifier" :ref="`${selection_identifier}_dock`" :data-holder="selection_identifier" />
     </div>
 </template>
 
 <script setup lang="ts">
     import { NuxtLink } from '#components'
-    
+
     const link = ref(<string>useRoute().query.site || 'https://www.example.com')
     const link_to_load = computed(() => `${useRequestURL().origin}/api/site?link=${encodeURIComponent(link.value)}`) // useRequestURL() needed for SSR to work; /api/site as a reative route does not work on the server.
     const selector = ref('html>body>div>h1~p')
-    
-    const tree_builder = ref(new DOMParser())
+    const tree_builder_source = await useAsyncData('tree_builder_source', async () => {
+        if (import.meta.env.SSR) {
+            const { JSDOM } = await import('jsdom')
+            return new JSDOM().window
+        } else {
+            return window
+        }
+    })
+    const tree_builder = ref(new (tree_builder_source.data.value!.DOMParser)())
     let node_identifier = 0
 
     const site_text = useFetch(link_to_load)
     const site = computed(() => tree_builder.value.parseFromString(<string>site_text.data.value, 'text/html').documentElement)
     const site_holder = computed(() => {
-        const holder = document.createElement('div')
+        const holder = tree_builder_source.data.value!.document.createElement('div')
         holder.attachShadow({ mode: 'open' })
         holder.shadowRoot!.appendChild(site.value)
         return holder
     })
     const selections = computed(() => Array.from(site.value.querySelectorAll(selector.value)))
-    const selection_holders = computed(() =>
+    const selection_holders = computed(() => (node_identifier = 0) || new Map(
         selections.value
             .map(selected => {
-                const holder = document.createElement('div')
+                const holder = tree_builder_source.data.value!.document.createElement('div')
                 holder.attachShadow({ mode: 'open' })
                 const clone = <HTMLElement>selected.cloneNode()
-                selected.computedStyleMap().forEach((value, key) => clone.style.setProperty(key, value[0].toString()))
+                Array.from(tree_builder_source.data.value!.getComputedStyle(selected)).forEach(([key, value]) => clone.style.setProperty(key, value))
                 holder.shadowRoot!.appendChild(clone)
-                holder.id = `selection-${node_identifier++}`
-                return holder
-            }))
+                return [holder.id = `selection_${node_identifier++}`, holder]
+            })))
+    const site_dock = useTemplateRef('site_dock')
+    const selection_docks = computed(() => new Map(Array.from(selection_holders.value.entries()).map(([selection_identifier, holder]) => [selection_identifier, <Readonly<Ref<HTMLElement>>>useTemplateRef(`${selection_identifier}_dock`)])))
+    onMounted(bind_holders)
+    watch(site_holder, bind_holders)
+    watch(selection_holders, bind_holders)
+    function bind_holders() {
+        site_dock.value!.appendChild(site_holder.value)
+        Array.from(selection_docks.value.entries()).forEach(([selection_identifier, dock]) => dock.value!.appendChild(selection_holders.value.get(selection_identifier)!))
+    }
 </script>
 
 <style scoped>
