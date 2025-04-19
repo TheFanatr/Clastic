28d17a45a9f806ab34d82f1848db53d7ce52c68e
Alexander David Fânaț-Dănuț <oaVa-o@outlook.com>
2025-04-19 07:39:01 +0300

    Fix many render bugs.

--- pages/index.vue
+++ pages/index.vue
@@ -9,15 +9,17 @@
         <label for="selector">Selector</label>
         <input type="text" id="selector" name="selector" v-model="selector" />
         <br />
-        <Dock v-for="[selection_identifier, { selection, styles }] in selections" :tree="selection" :styles="styles" :key="selection_identifier" />
-        <Dock :tree="site" :styles="null" />
+        <button v-if="!render_site" @click="render_site = true">Render</button>
+        <Dock v-if="render_selection" v-for="[selection_identifier, { selection, styles }] in selections" :tree="selection" :styles="styles" :key="selection_identifier" />
+        <Dock v-if="render_site" :tree="site" :styles="null" @vue:mounted="render_selection = true" />
     </div>
 </template>
 
 <script setup lang="ts">
-    import { NuxtLink } from '#components'
-    import Dock from '~/components/Dock.vue'
+    import { NuxtLink, Dock } from '#components'
     
+    const render_selection = ref(false)
+    const render_site = ref(false)
     const link = ref(<string>useRoute().query.site || 'https://www.example.com')
     const link_to_load = computed(() => `${useRequestURL().origin}/api/site?link=${encodeURIComponent(link.value)}`) // useRequestURL() needed for SSR to work; /api/site as a reative route does not work on the server.
     const selector = ref('html>body>div>h1')
